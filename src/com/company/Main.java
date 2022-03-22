f957cef0b0bd43a92b2756ab26687cd6c20705f7
unknown <knts2nttn@gmail.com>
2022-03-22 10:08:06 +0700

    baitap

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập số vào đây ");
+        int number = scanner.nextInt();
+        if (number<2){
+            System.out.println(number+" ko phải số nguyên tố");
+        }else {
+            int i =2;
+            boolean check= true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number + " là số nguyên tố");
+            else
+                System.out.println(number + " ko phải số nguyên tố");
+        }
+    }
+}
